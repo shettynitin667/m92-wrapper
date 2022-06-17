c90cc7250df68a7095156461925b5542589ff438
Nitin Shetty <nitin.shetty@freecharge.com>
2022-06-17 16:12:38 +0530

    Package changes

--- index.js
+++ index.js
@@ -1,37 +1,34 @@
 #!/usr/bin/env node
-const { Aes, AesUtils } =require('@m92/crypto');
+const M92 =require("./node_modules/@m92/crypto/dist");
 
 // eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types, @typescript-eslint/no-explicit-any
 function encryptPayload(payload, tokenValue) {
   if (!tokenValue) {
     return payload;
   }
-  const encryptionKey = AesUtils.extractKeyFromToken(tokenValue);
+  const encryptionKey = M92.AesUtils.extractKeyFromToken(tokenValue);
   const bodyString =
     typeof payload !== "string" ? JSON.stringify(payload) : payload;
   const encryptParams = {
     key: encryptionKey,
     data: bodyString,
   };
-  const encryptedDataObj = Aes.encrypt("aes-256-gcm", encryptParams);
+  const encryptedDataObj = M92.Aes.encrypt("aes-256-gcm", encryptParams);
   const { payload: encryptedData } = encryptedDataObj;
   return encryptedData;
 }
 
-function decryptPayload(
-  encryptedPayload,
-  tokenValue
-) {
+function decryptPayload(encryptedPayload, tokenValue) {
   if (!tokenValue) {
     return encryptedPayload;
   }
-  const decryptionKey = AesUtils.extractKeyFromToken(tokenValue);
+  const decryptionKey = M92.AesUtils.extractKeyFromToken(tokenValue);
   const decryptParams = { key: decryptionKey, payload: encryptedPayload };
-  const decryptedDataObj = Aes.decrypt("aes-256-gcm", decryptParams);
-  
+  const decryptedDataObj = M92.Aes.decrypt("aes-256-gcm", decryptParams);
+
   const { data: decryptedDataString } = decryptedDataObj;
   const decryptedData = JSON.parse(decryptedDataString);
   return decryptedData;
 }
 
-module.exports = {encryptPayload,decryptPayload}
+module.exports = { encryptPayload, decryptPayload }
